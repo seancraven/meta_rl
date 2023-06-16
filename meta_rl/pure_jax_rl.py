68338a36058a20bf2467794785af7195b528f508
seancraven <seanpietercraven@gmail.com>
2023-06-16 10:50:01 +0100

    sart purjaxrl

---
+++ meta_rl/pure_jax_rl.py
@@ -0,0 +1,14 @@
+import gymnax
+import haiku as hk
+import jax
+import jax.numpy as jnp
+import jaxtyping
+import numpy as np
+import optax
+import rlax
+
+
+class ActorCritic(hk.Module):
+
+
+    def __call__(self, state: )
